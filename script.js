96e9206d7190855cd2e17989cf25366c1aecc3e0
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-29 02:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 //complete this code
 class Rectangle {
-	constructor(width,height){
-		this.width=width;
-		this.height=height;
-	}
-	get width(){
-		return this.width;
-	}
-	get height(){
-		return this.height;
-	}
-	// Setters for width and height
-	set width(value) {
-	this._width = value;
-	}
-	
-	set height(value) {
-	this._height = value;
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	getArea(){
-		return this.width * this.height;
-	}
+  // Getters for width and height
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  // Setters for width and height
+  set width(value) {
+    this._width = value;
+  }
+
+  set height(value) {
+    this._height = value;
+  }
+
+  // Method to calculate the area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side){
-		super(side,side);
-	}
-	getPerimeter(){
-		return 4 * this.width;
-	}
+  constructor(side) {
+    super(side, side); // Call the constructor of the parent class (Rectangle) with the side as width and height
+  }
+
+  // Method to calculate the perimeter of the square
+  getPerimeter() {
+    return 4 * this._width; // In a square, all sides are equal, so perimeter = 4 * side
+  }
 }
+
 const rectangle = new Rectangle(4,5);
 console.log(rectangle.getArea());
 
